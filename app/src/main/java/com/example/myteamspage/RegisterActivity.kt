8b366ea745ee3e9c4aeb1e479435f1a19c58d8db
carlosmartins77 <carlosmatrins@gmail.com>
2023-05-20 16:14:15 +0100

    ADDED register

---
+++ app/src/main/java/com/example/myteamspage/RegisterActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myteamspage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val continueButton = findViewById<Button>(R.id.registerContinueBtn)
+        val emailEt = findViewById<EditText>(R.id.registerEmailEt)
+        val passwordEt = findViewById<EditText>(R.id.registerPasswordEt)
+        val confirmPasswordEt = findViewById<EditText>(R.id.registerConfirmPasswordEt)
+
+        continueButton.setOnClickListener {
+            val emailTemp = emailEt.text.toString()
+            val password = passwordEt.text.toString()
+            val confirmPassword = confirmPasswordEt.text.toString()
+
+            if(emailTemp.matches(Regex("/^\\S+@\\S+\\.\\S+\$/")))
+            {
+                val email = emailTemp
+            }
+
+
+        }
+    }
+}
