142c39807c83068c818d9cb463b25a5b922d4d3c
Joao Ricardo Pinto Azevedo <jpinto.azevedo@gmail.com>
2023-06-06 11:13:52 +0100

    [Update] - Publications screens

--- app/src/main/java/com/example/myteamspage/Activities/ComposeTweet.kt
+++ app/src/main/java/com/example/myteamspage/Activities/ComposeTweet.kt
@@ -1,12 +1,26 @@
 package com.example.myteamspage.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import com.example.myteamspage.Activities.Account.PersonalInfo
 import com.example.myteamspage.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ComposeTweet : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compose_tweet)
+
+        val arrowBack = findViewById<ImageView>(R.id.compose_tweet_arrow_icon)
+
+        arrowBack.setOnClickListener {
+            val intent = Intent(this@ComposeTweet, PublicationScreen::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/myteamspage/Activities/MyPublication.kt
+++ app/src/main/java/com/example/myteamspage/Activities/MyPublication.kt
@@ -3,39 +3,42 @@ package com.example.myteamspage.Activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myteamspage.Activities.Account.AccountOptions
 import com.example.myteamspage.Activities.Account.PersonalInfo
 import com.example.myteamspage.Adapters.PublicationAdapter
 import com.example.myteamspage.Classes.Publication
 import com.example.myteamspage.R
-import com.example.myteamspage.databinding.ActivityPublicationScreenBinding
+import com.example.myteamspage.databinding.ActivityMyPublicationBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class PublicationScreen : AppCompatActivity() {
-
+class MyPublication : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_publication)
 
-        val binding = ActivityPublicationScreenBinding.inflate(layoutInflater)
+        val binding = ActivityMyPublicationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.publication_screen_recyclerview)
-        val progressBar = findViewById<ProgressBar>(R.id.publication_progress_bar)
+        val progressBar = findViewById<ProgressBar>(R.id.my_publication_progress_bar)
+        val recyclerView = findViewById<RecyclerView>(R.id.my_publication_recyclerview)
 
-        val my_publication = findViewById<TextView>(R.id.publication_my_publication)
-        val featherPen = findViewById<ImageView>(R.id.publication_btn_feather)
+        val feed = findViewById<TextView>(R.id.my_publication_feed)
+        val featherPen = findViewById<ImageView>(R.id.my_publication_btn_feather)
 
-        // Put the progress value you want
-        progressBar.max = 100
-        progressBar.progress = 50
+        progressBar.max = 100 // Set the maximum value of the progress bar
 
+        val desiredProgress = 75 // Desired progress value between 50 and 100
+        val progressRange = 100 - 50 // Calculate the range between the minimum and maximum progress values
+
+        val adjustedProgress = (desiredProgress - 50) * progressRange / 50 // Adjust the desired progress within the range
+
+        progressBar.progress = adjustedProgress + 50
+
+        
         // Create a list of Publication objects
         val publicationList: List<Publication> = listOf(
             Publication(R.drawable.baseline_image_24, "User 1", "Tweet 1", R.drawable.baseline_gif_box_24, R.drawable.arrow_drop_down),
@@ -44,11 +47,13 @@ class PublicationScreen : AppCompatActivity() {
         )
 
         val publicationAdapter = PublicationAdapter(publicationList)
+
         recyclerView.adapter = publicationAdapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.my_publication_bottomNavigationView)
         bottomNavigationView.selectedItemId = R.id.bottom_nav_publication
+
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.bottom_nav_teams -> {
@@ -71,14 +76,16 @@ class PublicationScreen : AppCompatActivity() {
             }
         }
 
-        my_publication.setOnClickListener {
-            val intent = Intent(this@PublicationScreen, MyPublication::class.java)
+        featherPen.setOnClickListener {
+            val intent = Intent(this@MyPublication, ComposeTweet::class.java)
             startActivity(intent)
+            finish()
         }
 
-        featherPen.setOnClickListener {
-            val intent = Intent(this@PublicationScreen, ComposeTweet::class.java)
+        feed.setOnClickListener {
+            val intent = Intent(this@MyPublication, PublicationScreen::class.java)
             startActivity(intent)
+            finish()
         }
     }
 }
